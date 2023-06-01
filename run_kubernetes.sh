584d6932bfbf82d6b2e8cde09a4ee538cde3b881
nhung306 <yuki230698@gmail.com>
2023-06-02 06:32:13 +0700

    commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,22 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="nhungtrinh/udacity_project4:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity_project4\
+    --image=$dockerpath\
+    
+    --port=80 --labels app=udacity_project4\
+    --restart=Never
 
 
 # Step 3:
 # List kubernetes pods
+kubectl wait --for=condition=ready pod --all
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity_project4 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="nhungtrinh/udacity_project4:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag udacityproject4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+echo "Push image to a docker repository"
+docker push $dockerpath
